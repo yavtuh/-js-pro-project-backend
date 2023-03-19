73adbe43691654ad2958a540b37833f98aa81fb0
yavtuh <yavtushenko2018seriy@gmail.com>
2023-03-19 11:59:56 +0300

    module1-task2

--- generate-photos.js
+++ generate-photos.js
@@ -1,4 +1,4 @@
-const countOfOffers = 25;
+function generateData(){
 const countOfComments = 100;
 const minLikes = 15;
 const maxLikes = 200;
@@ -6,48 +6,59 @@ const descriptions = ['description1', 'description2', 'description3', 'descripti
 const name = ['test1', 'test2', 'test3', 'test4', 'test5', 'test6'];
 const messages = ['comments1', 'comments2', 'comments3', 'comments4', 'comments5', 'comments6', 'comments7', 'comments8', 'comments9', 'comments10'];
 const countAvatar = 6;
+const countRandomOfComments = 20;
+const fs = require("fs");
+const photosArray = fs.readdirSync("./public/photos/");
 
-function getRandomNumber(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
-function getRandomArray(array) {
-    const randomArrayNumber = getRandomNumber(0, array.length - 1);
-    return array[randomArrayNumber];
-}
 
-function getComment(index) {
-    const randomAvatarName = getRandomNumber(1, countAvatar);
-    return {
-        id: index + 1,
-        avatar: `img/avatar-${randomAvatarName}.svg`,
-        message: getRandomArray(messages),
-        name: name[randomAvatarName - 1],
-    };
-}
-
-function getRandomComments() {
-    const countRandomComments = getRandomNumber(1, 20);
-    let arrayRandomComments = [];
-    for (let i = 0; i < countRandomComments; i++) {
-        arrayRandomComments.push(comments[getRandomNumber(0, comments.length - 1)].id);
+    function getRandomNumber(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-    return arrayRandomComments;
-}
 
-function getOffer(index) {
+    function getRandomArray(array) {
+        const randomArrayNumber = getRandomNumber(0, array.length - 1);
+        return array[randomArrayNumber];
+    }
+
+    function getComment(index) {
+        const randomAvatarName = getRandomNumber(1, countAvatar);
+        return {
+            id: index + 1,
+            avatar: `img/avatar-${randomAvatarName}.svg`,
+            message: getRandomArray(messages),
+            name: name[randomAvatarName - 1],
+        };
+    }
+
+    function getRandomComments() {
+        const countRandomComments = getRandomNumber(1, countRandomOfComments);
+        let arrayRandomComments = [];
+        for (let i = 0; i < countRandomComments; i++) {
+            arrayRandomComments.push(comments[getRandomNumber(0, comments.length - 1)].id);
+        }
+        return arrayRandomComments;
+    }
+
+    function getOffer(e, index) {
+        return {
+            id: index + 1,
+            url: `http://localhost:4000/photos/${e}`,
+            description: getRandomArray(descriptions),
+            likes: getRandomNumber(minLikes, maxLikes),
+            comments: getRandomComments(),
+        };
+    }
+
+    const comments = new Array(countOfComments).fill(null).map((e, index) => getComment(index));
+    const photos = photosArray.map((e, index) => getOffer(e, index));
     return {
-        id: index + 1,
-        url: `photos/${index + 1}.jpg`,
-        description: getRandomArray(descriptions),
-        likes: getRandomNumber(minLikes, maxLikes),
-        comments: getRandomComments(),
-    };
+        comments: comments,
+        photos: photos
+    }
 }
 
-const comments = new Array(countOfComments).fill(null).map((e, index) => getComment(index));
-const photos = new Array(countOfOffers).fill(null).map((e, index) => getOffer(index));
 
-module.exports = {comments, photos};
+module.exports = {generateData};
--- main.js
+++ main.js
@@ -1,29 +1,76 @@
-const  {comments, photos} = require ("./generate-photos.js");
+const  {generateData} = require ("./generate-photos.js");
 
 const http = require("http");
 const fs = require("fs");
+const path = require('path')
+const busboy = require('busboy');
 
-
-fs.writeFileSync("photos.txt", JSON.stringify(photos));
-fs.writeFileSync("comments.txt", JSON.stringify(comments));
+const data = generateData();
+fs.writeFileSync("photos.txt", JSON.stringify(data.photos));
+fs.writeFileSync("comments.txt", JSON.stringify(data.comments));
 http.createServer(function (request, response) {
     response.writeHead(200, {"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"});
     const url = request.url;
+    if (request.method === "GET"){
+    
+        if(url === "/photos"){
+            const dataPhotos = fs.readFileSync("photos.txt", "utf-8");
 
-    if(url === "/photos"){
-        const dataPhotos = fs.readFileSync("photos.txt", "utf-8");
+            response.write(dataPhotos);
+            response.end();
+        }
 
-        response.write(dataPhotos);
-        response.end();
+        if(url === "/comments"){
+            const dataComments = fs.readFileSync("comments.txt", "utf-8");
+
+            response.write(dataComments);
+            response.end();
+        }
+    } else if(request.method === "POST"){
+        if(url === "/sendform"){
+            let filename = '';
+            const bb = busboy({ headers: request.headers });
+                bb.on('file', (name, file, info) => {
+                filename = info.filename;
+                const saveTo = path.join(__dirname, 'public/photos/', filename);
+                file.pipe(fs.createWriteStream(saveTo));
+                
+            });
+            bb.on('close', () => {
+                console.log(filename);
+                const data = generateData();
+                fs.writeFileSync("photos.txt", JSON.stringify(data.photos));
+                fs.writeFileSync("comments.txt", JSON.stringify(data.comments));
+                const newPhoto = data.photos.find(photo => photo.url === `http://localhost:4000/photos/${filename}`);  
+                response.write(JSON.stringify([newPhoto]));
+                response.end();
+            });
+            request.pipe(bb);
+        }
+    }
+    
+
+    let filePath = path.join(__dirname, 'public', url)
+
+    let contentType = 'text/html';
+
+    let mimeType = path.extname(filePath)
+
+    switch (mimeType) {
+        case '.png': contentType = 'image/png'; break;
+        case '.jpg': contentType = 'image/jpg'; break;
+        case '.jpeg': contentType = 'image/jpeg'; break;
     }
 
-    if(url === "/comments"){
-        const dataComments = fs.readFileSync("comments.txt", "utf-8");
-
-        response.write(dataComments);
-        response.end();
-    }
+    fs.readFile(filePath, (error, data) => {
+        if (error) return
+        
+        response.writeHead(200, { 'Content-Type':  contentType })
+        response.end(data, 'utf8')
+    
+    })
 
 }).listen(4000, function () {
     console.log("listen port 4000");
 });
+
